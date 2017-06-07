d227aad5bb75a2470e74bcf957c88c3765815e20
Joseph Joice <joseph_joice@intuit.com>
2017-06-07 03:44:37 -0700

    Removed proxy protocol from nginx conf

--- cookbooks/authoring/attributes/default.rb
+++ cookbooks/authoring/attributes/default.rb
@@ -1,8 +1,5 @@
 default['nginx']['tenantName'] = "care"
 default['nginx']['install_folder'] = "/usr/share/nginx/html/"
-default['wp']['dbName'] = "wp1"
-default['wp']['dbUser'] = "user1"
-default['wp']['dbPassword'] = "password"
-default['wp']['dbHost'] = "mydbinstance-wordpress.cn7kuj49ihkd.us-west-2.rds.amazonaws.com:3306"
+default['wp']['dbHost'] = "cms-wordpress-authoring-db-dev-us-west-2.cgtsxfmpe5va.us-west-2.rds.amazonaws.com:3306"
 default['deploy']['download_dir'] = "/deploy/artifacts"
 default['deploy']['plugins'] = [ "content-transformer" , "Plugin-CustomField" ]
