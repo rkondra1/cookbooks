6fb99a8bde5171130e3dc1985c049837aca3351e
Joseph Joice <joseph_joice@intuit.com>
2017-06-13 04:11:56 -0700

    Added saml plugin to attributes list

--- cookbooks/authoring/attributes/default.rb
+++ cookbooks/authoring/attributes/default.rb
@@ -2,4 +2,4 @@ default['nginx']['tenantName'] = "care"
 default['nginx']['install_folder'] = "/usr/share/nginx/html/"
 default['wp']['dbHost'] = "cms-wordpress-authoring-db-dev-us-west-2.cgtsxfmpe5va.us-west-2.rds.amazonaws.com:3306"
 default['deploy']['download_dir'] = "/deploy/artifacts"
-default['deploy']['plugins'] = [ "content-transformer" , "Plugin-CustomField", "utility-client" ]
+default['deploy']['plugins'] = [ "content-transformer" , "Plugin-CustomField", "utility-client", "plugin-saml-20-single-sign-on" ]
