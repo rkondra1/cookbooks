f9e18947dbb62f80f1cffe107799beecb4c23326
Joseph Joice <joseph_joice@intuit.com>
2017-06-12 23:17:10 -0700

    Added opstest

--- cookbooks/authoring/recipes/default.rb
+++ cookbooks/authoring/recipes/default.rb
@@ -3,14 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe "authoring::nginx"
 include_recipe "authoring::php-fpm"
 include_recipe "authoring::wp"
 include_recipe "authoring::plugins"
-
-service "nginx" do
-  action :restart
-end
 
 service "php-fpm" do
   action :restart
