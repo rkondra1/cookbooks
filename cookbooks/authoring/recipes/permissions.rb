94b0718554916566e5426c143bf3120acbda29e8
Joseph Joice <joseph_joice@intuit.com>
2017-07-14 12:44:27 +0530

    Adding permissions apply for html folder, dev no jms-client deploy

---
+++ cookbooks/authoring/recipes/permissions.rb
@@ -0,0 +1,11 @@
+#Execute the permissions change for html folder
+execute 'chown nginx:nginx -R html folder' do
+  command "chown -R nginx:nginx #{node['wp-authoring']['nginx']['install_folder']}"
+  user "root"
+end
+
+execute 'chmod nginx:nginx -R html folder' do
+  command "chmod  -R 755 #{node['wp-authoring']['nginx']['install_folder']}"
+  user "root"
+end
+
