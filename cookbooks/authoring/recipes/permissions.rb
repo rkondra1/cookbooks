741a8238ae573a6320f0df7593fcfafe2cd0acda
Joseph Joice <joseph_joice@intuit.com>
2017-08-30 00:53:12 -0700

    fix for single back slash

--- cookbooks/authoring/recipes/permissions.rb
+++ cookbooks/authoring/recipes/permissions.rb
@@ -4,9 +4,9 @@ execute 'chown nginx:nginx -R html folder' do
 end
 
 execute 'chmod to 644 for all files' do
-  command "find #{node['wp-authoring']['nginx']['install_folder']} -type f -exec chmod 644 {} \;"
+  command "find #{node['wp-authoring']['nginx']['install_folder']} -type f -exec chmod 644 {} \\;"
 end
 
 execute 'chmod to 744 for all directories' do
-  command "find #{node['wp-authoring']['nginx']['install_folder']} -type d -exec chmod 744 {} \;"
+  command "find #{node['wp-authoring']['nginx']['install_folder']} -type d -exec chmod 744 {} \\;"
 end
