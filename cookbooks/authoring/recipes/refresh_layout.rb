6edbae4492f99a1bb2dca5f748a6c47ad5dbfded
Joseph Joice <joseph_joice@intuit.com>
2017-09-21 05:50:38 -0700

    OICMS-1282 Wordpress version change to 4.8.2

--- cookbooks/authoring/recipes/refresh_layout.rb
+++ cookbooks/authoring/recipes/refresh_layout.rb
@@ -1,6 +1,7 @@
 #Refresh cas layouts
+
 execute "refresh cas layout" do
-  command "curl -o /dev/null -s -w \"%{http_code}\" -X POST localhost:#{node['nginx']['server']['healthcheck']['listen_port']}/cms/wordpress/?rest_route=/intuitcf/v1/notify|grep 200"
+  command "curl  -o /dev/null -s -w \"%{http_code}\" -X POST localhost:#{node['nginx']['server']['healthcheck']['listen_port']}/cms/wordpress/?rest_route=/intuitcf/v1/notify|grep 200"
   user 'nginx'
   retries 5
 end
